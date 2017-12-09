bb93cf8d321fb0de334ce293cc59d15b865b9a73
Roman Kushnarenko <sromku@gmail.com>
2017-12-09 22:33:18 +0200

    Update script to run tests

--- scripts-part-3/build.sh
+++ scripts-part-3/build.sh
@@ -45,7 +45,15 @@ fi
 # -----------------------------------------------------------------
 # -------------------------- TESTS & LINT--------------------------
 # -----------------------------------------------------------------
+# lint
 ./gradlew lint
+
+# run junit test
+if [ $buildType = 'debug' ]; then
+    ./gradlew testDebugUnitTest --stacktrace
+elif [ $buildType = 'release' ]; then
+    ./gradlew testReleaseUnitTest --stacktrace
+fi
 
 # -----------------------------------------------------------------
 # -------------------------- POST BUILD ---------------------------
@@ -55,6 +63,7 @@ rm -r artifacts/
 rm -r report/
 mkdir artifacts
 mkdir report
+mkdir report/test-results
 
 # copy apk to artifacts
 if [ ! -e "app/build/outputs/apk/$buildType/$apkFileName" ]; then
@@ -69,4 +78,26 @@ if [ ! -e "app/build/reports/lint-results.xml" ]; then
 	exit 1
 fi
 cp app/build/reports/lint-results.xml report/
+
+# copy tests results from all modules
+modules=("app" "common")
+for module in "${modules[@]}"
+do
+
+    testsDir=""
+    if [ $buildType = 'debug' ]; then
+        testsDir="$module/build/test-results/testDebugUnitTest"
+    elif [ $buildType = 'release' ]; then
+        testsDir="$module/build/test-results/testReleaseUnitTest"
+    fi
+
+    if [ ! "$(ls -A $testsDir)" ]; then
+        echo "Unit tests report wasn't found for module: $module"
+        continue
+    fi
+
+    # copy all files inside, to our folder
+    cp $testsDir/* report/test-results/
+
+done
 
